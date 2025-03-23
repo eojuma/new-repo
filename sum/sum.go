4cc85a44f8943b95b6c15bfadd28fb23ba55ff7f
Magolo <evansodhiambo658@gmail.com>
2025-03-23 11:08:46 +0300

    modulo

--- sum/sum.go
+++ sum/sum.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	a:=12
-	b:=24
+	a := 12
+	b := 24
 	fmt.Println(sum(a, 8))
 	fmt.Println(mult(2, b))
 	fmt.Println(subt(a, b))
 	fmt.Println(div(23.4, 7.5))
+	fmt.Println(mod(a, b))
 
 }
 
@@ -16,14 +17,18 @@ func sum(a, b int) int {
 	return a + b
 }
 
-func mult(a,b int)int{
-	return a*b
+func mult(a, b int) int {
+	return a * b
 }
 
-func subt(a,b int)int{
-	return a-b
+func subt(a, b int) int {
+	return a - b
 }
 
-func div(a,b float64)float64{
-	return a/b
+func div(a, b float64) float64 {
+	return a / b
+}
+
+func mod(a, b int) int {
+	return a % b
 }
