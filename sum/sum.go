1a208a744808d0690cfd8889ce34f16b70cdfd00
Magolo <evansodhiambo658@gmail.com>
2025-03-23 11:04:26 +0300

    updates

--- sum/sum.go
+++ sum/sum.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	a:=12
-	b:=24
+	a := 12
+	b := 24
 	fmt.Println(sum(a, 8))
 	fmt.Println(mult(2, b))
 	fmt.Println(subt(a, b))
@@ -16,14 +16,14 @@ func sum(a, b int) int {
 	return a + b
 }
 
-func mult(a,b int)int{
-	return a*b
+func mult(a, b int) int {
+	return a * b
 }
 
-func subt(a,b int)int{
-	return a-b
+func subt(a, b int) int {
+	return a - b
 }
 
-func div(a,b float64)float64{
-	return a/b
+func div(a, b float64) float64 {
+	return a / b
 }
