8af0d6ba4d8a85cb59eb212daf07f9e3b378ea79
Magolo <evansodhiambo658@gmail.com>
2025-03-24 09:59:36 +0300

    sum.go

--- sum/sum.go
+++ sum/sum.go
@@ -33,14 +33,3 @@ func mod(a, b int) int {
 	return (a % b)
 	// returns the remainder
 }
-
-// The workflow is amazing and by the end of the month I
-//  should be able to understand at least 85 per cent of information pertaining Git.
-
-//They say that coding needs critical thinking not fast hands but then i think it neds both critical thinking and fast hands too.
-//What do you think?
-
-//The first attempt to anything new is difficult,but then as days go by things get easier,clearer and eventually very familiar.
-//Do agree?
-
-///I am almost getting better at merging or rather getting the whole concept of branching.
